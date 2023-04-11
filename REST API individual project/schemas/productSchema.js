3edc6c23127162e228e56f8c56819d3e06fe7dff
Mikael Krefors <krefors@hotmail.com>
2023-04-11 16:15:11 +0200

    Minor changes, deleted some files

--- REST API individual project/schemas/productSchema.js
+++ REST API individual project/schemas/productSchema.js
@@ -5,7 +5,7 @@ const productSchema = new Schema({
   name:             { type: String, required: true },
   description:      { type: String, required: true },
   price:            { type: Number, required: true },
-  imgURL:         { type: String, required: true },
+  imgURL:           { type: String, required: true },
 
 });
 
