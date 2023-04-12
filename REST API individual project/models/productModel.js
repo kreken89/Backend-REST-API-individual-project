dd6a591292f3041d5ff133ecf050124f76022dff
Mikael Krefors <krefors@hotmail.com>
2023-04-12 12:14:39 +0200

    Added some comments

--- REST API individual project/models/productModel.js
+++ REST API individual project/models/productModel.js
@@ -2,34 +2,34 @@ const Product = require('../schemas/productSchema');
 
 exports.createNewProduct = async (req, res) => {
 
-    const { name, description, price, imgURL } = req.body;
+    const { name, description, price, imgURL } = req.body;                                          // Extracting data from request body
 
-    if( !name || !description || !price || !imgURL ) {
+    if( !name || !description || !price || !imgURL ) {                                              // Checking for missing fields in request body
         return res.status(400).json({ message: 'You need to enter all the fields' })
     }
 
-    const product = await Product.create({ name, description, price, imgURL });
+    const product = await Product.create({ name, description, price, imgURL });                     // Creating a new product object
 
-    if(!product) {
+    if(!product) {                                                                                  // Handling errors during product creation
         return res.status(500).json({ message: 'Something went wrong when creating the product' })
     }
 
-    res.status(201).json(product)
+    res.status(201).json(product)                                                                   // Sending successful response with created product object
 }
 
 exports.getProducts = async (req, res) => {
 
-    try {
+    try {                                                                                           // Fetching all products from the database
         const products = await Product.find()
-        res.status(200).json(products)     
+        res.status(200).json(products)                                                              // Sending successful response with fetched products  
     } catch (err) {
-        res.status(500).json({ message: 'Something went wrong when fetching the products' })
+        res.status(500).json({ message: 'Something went wrong when fetching the products' })        // Handling errors during product fetching
     }
-
 }
 
 exports.getProductsById = async (req, res) => {
-  try {
+
+  try {                                                                                             // Fetching a product by its ID from the database
     const product = await Product.findById(req.params.id);
     res.status(200).json(product);
   } catch (err) {
@@ -38,21 +38,20 @@ exports.getProductsById = async (req, res) => {
 }
 
 exports.updateProduct = async (req, res) => {
-  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  if (!product) {
+
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true, });         // Updating a product by its ID with data from request body
+  if (!product) {                                                                                   // Handling case where product is not found
     return res.status(404).json({ message: 'Could not find that product' });
   }
-  res.status(200).json(product);
+  res.status(200).json(product);                                                                    // Sending successful response with updated product
 };
 
 exports.deleteProduct = async (req, res) => {
-  const product = await Product.findByIdAndDelete(req.params.id);
-  if (!product) {
+  const product = await Product.findByIdAndDelete(req.params.id);                                   // Deleting a product by its ID from the database
+  if (!product) {                                                                                   // Handling case where product is not found
     return res.status(404).json({ message: 'Could not find that product' });
   }
-  res.status(200).json(product);
+  res.status(200).json(product);                                                                    // Sending successful response with deleted product
 };
 
 
